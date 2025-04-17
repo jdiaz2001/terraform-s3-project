6dd1b055ff5e7267611eade10a984b254b883c2c
Javier Diaz <jdiaz20@uh.edu>
2025-04-17 18:39:54 -0500

    update

--- variables.tf
+++ variables.tf
@@ -5,21 +5,21 @@ variable "aws_region" {
 
 variable "profile" {
   type        = string
-  description = "AWS Account Profile"
+  description = "AWS SSO Profile to be used"
 }
 
 variable "parent_domain" {
   type        = string
-  description = "The domain name for the website."
+  description = "The parent domain name for the website"
 }
 
 variable "subdomain" {
   type        = string
-  description = "The domain name for the website."
+  description = "The subdomain name for the website"
 }
 
 variable "route53_zone_id" {
   type        = string
-  description = "The domain name for the website."
+  description = "Domain Hosted Zone ID"
 }
 
